ae96a2d01dc8b99adaf517c3770f94ef4713b169
rainrisa <risarainly@gmail.com>
2025-01-09 12:03:00 +0700

    feat: reduce whitespaces

--- src/extra/format.ts
+++ src/extra/format.ts
@@ -1,5 +1,5 @@
 import { Prisma } from "@prisma/client";
 
 export default function format(tasks: Prisma.TaskGetPayload<{}>[]) {
-  return tasks.map((task) => `${task.text} [${task.id}]`).join("\n\n");
+  return tasks.map((task) => `${task.text} [${task.id}]`).join("\n");
 }
